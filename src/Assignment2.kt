ad67dd287fc385440a5fb18a71b1b6b7d07db8e1
gushimaaceline <celinegushimaa@gmail.com>
2021-03-05 09:16:43 +0300

    first commit

--- src/Assignment2.kt
+++ src/Assignment2.kt
@@ -1,27 +1,47 @@
-fun main() {
-    var myName= "Hello Celine"
-    println(myName)
+fun main (){
+println("Hello Celine")
     var result= modulus(12,10)
     println(result)
-var sum= add(30,40,20,15)
+    var sum = add(15,20, 12, 25)
     println(sum)
-    var inFact= "I am courageous"
-    println(inFact)
+    var inFact= "I am curious"
+println(inFact)
+    var sub= difference(15,9)
+    println(sub)
 }
-fun name(myName:String):String {
-    var myName= "Hello Celine"
+fun name (myName: String):String {
+ var myName= "Celine"
     return myName
-
 }
-fun modulus(a: Int, b: Int):Int {
+fun modulus(a:Int,b:Int):Int {
     var result= a%b
     return result
 }
-fun add(a:Int, b:Int, c:Int, d:Int) :Int{
-    var sum=a+b+c+d
+fun add (a:Int,b: Int,c:Int,d:Int):Int {
+    var sum= a+b+c+d
     return sum
 }
-fun interestFact(inFact:String) :String {
-var inFact= "I am courageous"
+fun interestFact (inFact: String):String {
+    var inFact= "I am curious"
     return inFact
 }
+fun difference(a: Int,b: Int):Int {
+    var sub = a-b
+    return sub
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
